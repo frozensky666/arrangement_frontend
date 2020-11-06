1e1cf546940eaceddda1412c2d923f59defb209f
sky <171180575@smail.nju.edu.cn>
2020-11-06 13:17:05 +0800

    删除了一些多余的文件

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,5 @@
 const TEST_GET = "/api/test/get";
 const TEST_POST = "/api/test/post";
-const COM_SEARCH = "/comsearch/document";
 const RESOURCE_GANTT_HOUR = "/resourceGantt/hour";
 const RESOURCE_GANTT_DAY = "/resourceGantt/date";
 
@@ -8,7 +7,6 @@ const RESOURCE_GANTT_DAY = "/resourceGantt/date";
 module.exports = {
   TEST_GET,
   TEST_POST,
-  COM_SEARCH,
   RESOURCE_GANTT_DAY,
   RESOURCE_GANTT_HOUR
 };
