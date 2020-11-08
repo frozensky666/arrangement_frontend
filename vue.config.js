5a7f1bfc011b9f99119218b43d4b94b09103d25f
GStarP <1700066935@qq.com>
2020-11-08 20:08:57 +0800

    chore: 添加联调所需配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   configureWebpack: {
-    devServer: {}
+    devServer: {
+      port: 8085,
+      open: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8089',
+          ws: true,
+          changeOrigin: true,
+          pathRewrite:{
+            '^/api': '/'
+          }
+        }
+      }
+    }
   }
 };
